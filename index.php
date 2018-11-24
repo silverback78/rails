2ca2513b4b920cbe318b6b4a3d3a74fca001060b
silverback78 <jmcgilberry@gmail.com>
2018-11-24 13:42:14 -0600

    Modified to use rest verbs

--- index.php
+++ index.php
@@ -12,14 +12,9 @@ Flight::route('GET /get/@key', function($key) {
     Flight::json($sessionData);
 });
 
-Flight::route('GET /setString/@key/@value', function($key, $value) {
-    $_SESSION[$key] = $value;
-    Flight::json($_SESSION[$key]);
-});
-
-Flight::route('POST /set', function() {
-    $key = Utils::GetPostVar('key');
-    $value = Utils::GetPostVar('value');
+Flight::route('PUT /set', function() {
+    $key = Flight::request()->data->key;
+    $value = Flight::request()->data->value;
 
     if (Utils::IsNullOrWhitespace($value) && isset($_SESSION[$key])) {
         unset($_SESSION[$key]);
